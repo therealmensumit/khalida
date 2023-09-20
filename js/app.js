9533e38682948ca9da50e00845a74521a3e273ef
therealmensumit <sk03950@gmail.com>
2023-09-20 19:12:01 +0530

    first-commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(function () {
+  $("#header").load("include/header.html");
+  $("#footer").load("include/footer.html");
+});
+
+$(".product-slider").slick({
+  dots: true,
+  arrow: true,
+  infinite: false,
+  speed: 300,
+  // centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: ".prev-arrow",
+  nextArrow: ".next-arrow",
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        arrows: true,
+        slidesToScroll: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+$("#yt-vid").grtyoutube({
+  autoPlay: true,
+});
