08307e1b0830e3c590311638a89f40d178198cff
therealmensumit <sk03950@gmail.com>
2023-09-21 13:49:22 +0530

    about page done

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(".product-slider").slick({
   centerPadding: "60px",
   slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay: true,
+  autoplay: false,
   autoplaySpeed: 2000,
   prevArrow: ".prev-arrow",
   nextArrow: ".next-arrow",
@@ -31,6 +31,53 @@ $(".product-slider").slick({
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+$(".testimonial-slider").slick({
+  dots: true,
+  arrow: true,
+  infinite: false,
+  speed: 300,
+  // centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  prevArrow: ".prev-arrow",
+  nextArrow: ".next-arrow",
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        arrows: true,
+        slidesToScroll: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
       },
